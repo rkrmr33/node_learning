1fcccfb482374949d3fa4e8ce7823cc3d69764d2
Roi Kramer <roikramer120@gmail.com>
2020-01-09 16:16:13 +0200

    EventEmitter class: added more tests and functions

--- nodejs/my-emitter/EventEmitter.js
+++ nodejs/my-emitter/EventEmitter.js
@@ -1,17 +1,23 @@
 
 const defaultEvents = ['error', 'newListener', 'removeListener'];
 
-function notifyListeners(eventName, eventObj, thisValue, ...args) {
+function notifyListeners(eventObj, thisValue, ...args) {
     const _eventObj = eventObj;
     const { listeners, onceListeners } = _eventObj;
     const allListeners = [...listeners, ...onceListeners];
 
-    for (let i = 0; i < allListeners; i += 1) {
-        const listener = listeners[i];
+    if (!allListeners.length) {
+        return false;
+    }
+
+    for (let i = 0; i < allListeners.length; i += 1) {
+        const listener = allListeners[i];
         listener.call(thisValue, ...args);
     }
 
     _eventObj.onceListeners = []; // get rid of all once listeners
+
+    return true;
 }
 
 function addNewEvent(events, newEventName) {
@@ -31,9 +37,44 @@ function isEmptyEvent(eventObj) {
 
 function checkListenerCount(eventName, listenerCount, limit) {
     if (listenerCount > limit) {
-        console.warn(`max listener limit (${limit}) exceeded 
-            for event: ${eventName}, currently at: ${listenerCount}`);
+        console.warn(`max listener limit (${limit}) exceeded for event: '${eventName}',`
+            + ` currently at: ${listenerCount}. You should probably check for memory leaks.`);
     }
+}
+
+function addNewListeners(emitter, eventName, isOnceListeners, ...listeners) {
+    if (typeof eventName !== 'string' && typeof eventName !== 'symbol') {
+        throw new TypeError('eventName argument must be of type <string> or <symbol>');
+    }
+
+    if (listeners.length === 0) {
+        throw new Error('must provide at least one listener object');
+    }
+
+    if (!listeners.every((listener) => typeof listener === 'function')) {
+        throw new TypeError('all listeners must be of type <function>');
+    }
+
+    const { _events } = emitter;
+
+    let eventObj = _events[eventName];
+
+    // new event
+    if (!eventObj) {
+        eventObj = addNewEvent(_events, eventName);
+    }
+
+    // notify of new listeners
+    notifyListeners(_events.newListener, emitter, eventName, ...listeners);
+
+    // add listeners
+    if (isOnceListeners) {
+        eventObj.onceListeners.push(...listeners);
+    } else {
+        eventObj.listeners.push(...listeners);
+    }
+
+    checkListenerCount(eventName, emitter.listenerCount(eventName), emitter.getMaxListeners());
 }
 
 class EventEmitter {
@@ -44,37 +85,37 @@ class EventEmitter {
         defaultEvents.forEach((defEvent) => addNewEvent(this._events, defEvent));
     }
 
+    /**
+     * add a listener (or multiple listeners) to a specific event
+     * @param {String, Symbol} eventName the name of the event
+     * @param  {...Function} listeners at least one listener
+     * @returns {EventEmitter} this event emitter
+     */
     on(eventName, ...listeners) {
-        if (typeof eventName !== 'string' && typeof eventName !== 'symbol') {
-            throw new TypeError('eventName argument must be of type <string> or <symbol>');
-        }
-
-        if (listeners.length === 0) {
-            throw new Error('must provide at least one listener object');
-        }
-
-        if (!listeners.every((listener) => typeof listener === 'function')) {
-            throw new TypeError('all listeners must be of type <function>');
-        }
-
-        let eventObj = this._events[eventName];
-
-        // new event
-        if (!eventObj) {
-            eventObj = addNewEvent(this._events, eventName);
-        }
-
-        // notify of new listeners
-        notifyListeners('newListener', this._events.newListener, this, [...listeners]);
-
-        // add listeners
-        eventObj.listeners.push(listeners);
-
-        checkListenerCount(eventName, this.listenerCount(eventName), this.getMaxListeners());
+        addNewListeners(this, eventName, false, ...listeners);
 
         return this;
     }
 
+    /**
+     * Add a listener (or multiple listeners) to a specific event.
+     * The listener(s) will be removed after a single event has been emitted
+     * @param {String, Symbol} eventName the name of the event
+     * @param  {...Function} listeners at least one listener
+     * @returns {EventEmitter} this event emitter
+     */
+    once(eventName, ...listeners) {
+        addNewListeners(this, eventName, true, ...listeners);
+
+        return this;
+    }
+
+    /**
+     * emit a specific event and call all of its listeners
+     * @param {String, Symbol} eventName the name of the event to be emitted
+     * @param  {...any} args any arguments to be passed to the listeners
+     * @returns {Boolean} true if there are listeners and false otherwise
+     */
     emit(eventName, ...args) {
         const eventObj = this._events[eventName];
 
@@ -82,11 +123,23 @@ class EventEmitter {
             return false; // no listeners for that event
         }
 
-        notifyListeners(eventName, eventObj, this, ...args);
+        const res = notifyListeners(eventObj, this, ...args);
+
+        if (!res && eventName === 'error') {
+            throw new Error('no listeners on \'error\' event');
+        }
+
+        if (isEmptyEvent(eventObj) && !defaultEvents.includes(eventName)) {
+            delete this._events[eventName]; // remove an event with no listeners (if not default event)
+        }
 
         return true;
     }
 
+    /**
+     * @param {String, Symbol} eventName the name of the event
+     * @returns {Number} the number of listeners for the specified event
+     */
     listenerCount(eventName) {
         const eventObj = this._events[eventName];
 
@@ -99,15 +152,51 @@ class EventEmitter {
         return listeners.length + onceListeners.length;
     }
 
+    /**
+     * @returns {Number} the number of max listeners per event for
+     * this EventEmitter instance.
+     */
     getMaxListeners() {
         return this._maxListeners;
     }
 
+    /**
+     * Changes the amount of listeners allowed per event.
+     * This is not a hard limit, going beyond the limit simply
+     * prints a warning message that might indicate on memory leaks.
+     * The given number is truncated to the nearest positive integer.
+     * 0 / Infinity meaning unlimited listeners.
+     * @param {Number} n the new maximum limit.
+     */
     setMaxListeners(n) {
-        // TODO: check for range
-        this._maxListeners = n;
+        if (typeof n !== 'number') {
+            throw new TypeError('the argument must be of type <number>');
+        }
+
+        let _n = Number.parseInt(n, 10);
+        if (_n <= 0 || _n > Number.MAX_SAFE_INTEGER) {
+            _n = Infinity;
+        }
+
+        this._maxListeners = _n;
 
         return this;
+    }
+
+    /**
+     * receive a copy on the listeners of the specified event
+     * @param {String, Symbol} eventName the name of the event
+     * @return {[Function]} returns an array with all the listeners
+     * of the specified event
+     */
+    listeners(eventName) {
+        const eventObj = this._events[eventName];
+
+        if (!eventObj) { return []; } // no listeners for that event
+
+        const { listeners, onceListeners } = eventObj;
+
+        return [...listeners, ...onceListeners];
     }
 }
 
--- nodejs/my-emitter/__tests__/events.spec.js
+++ nodejs/my-emitter/__tests__/events.spec.js
@@ -49,6 +49,214 @@ describe('EventEmitter Tests', () => {
 
             emitter.on('test', () => {});
             expect(emitter.listenerCount('test')).toBe(1);
+
+            emitter.on('test', () => {});
+            expect(emitter.listenerCount('test')).toBe(2);
+        });
+
+        test('should return the same emitter', () => {
+            const emitter = new EventEmitter();
+            const emitter2 = emitter.on('x', () => {});
+
+            expect(emitter2).toEqual(emitter);
+        });
+
+        test('try wrong argument types', () => {
+            const emitter = new EventEmitter();
+
+            expect(() => {
+                emitter.on(1, () => {});
+            }).toThrowError(new TypeError('eventName argument must be of type <string> or <symbol>'));
+
+            expect(() => {
+                emitter.on('asd');
+            }).toThrowError(new Error('must provide at least one listener object'));
+
+            expect(() => {
+                emitter.on('asd', () => {}, 1);
+            }).toThrowError(new TypeError('all listeners must be of type <function>'));
+        });
+
+        test('should print max listeners acceeded warning message to console.warn', () => {
+            let warningMessage;
+            const consoleWarnMock = (msg) => { warningMessage = msg; };
+            const consoleWarn = console.warn;
+            const emitter = new EventEmitter();
+            const limit = emitter.getMaxListeners();
+            const eventName = 'error';
+
+            console.warn = consoleWarnMock;
+
+            for (let i = 0; i < limit + 1; i += 1) {
+                emitter.on(eventName, () => {});
+            }
+
+            expect(warningMessage).toBe(`max listener limit (${limit}) exceeded for event: '${eventName}',`
+            + ` currently at: ${limit + 1}. You should probably check for memory leaks.`);
+
+            console.warn = consoleWarn;
+        });
+    });
+
+    /**
+     * emitter.once()
+     */
+    describe('testing emitter.once() method', () => {
+        test('should be able to add once listener', () => {
+            const emitter = new EventEmitter();
+            let wasCalled = false;
+            expect(emitter.listenerCount('test')).toBe(0);
+
+            emitter.once('test', () => { wasCalled = true; });
+            expect(emitter.listenerCount('test')).toBe(1);
+
+            emitter.emit('test');
+            expect(wasCalled).toBeTruthy();
+        });
+
+        test('should remove once listener after one emission', () => {
+            const emitter = new EventEmitter();
+
+            emitter.once('test', () => {});
+            emitter.emit('test');
+            expect(emitter.listenerCount('test')).toBe(0);
+        });
+    });
+
+    /**
+     * emitter.emit()
+     */
+    describe('testing emitter.emit() method', () => {
+        test('should return false when emitting empty event', () => {
+            const emitter = new EventEmitter();
+
+            const result = emitter.emit('x');
+            expect(result).toBeFalsy();
+        });
+
+        test('should return true when emitting event with listeners', () => {
+            const emitter = new EventEmitter();
+
+            emitter.on('x', () => {});
+
+            const result = emitter.emit('x');
+            expect(result).toBeTruthy();
+        });
+
+        test('should call the listener', () => {
+            const emitter = new EventEmitter();
+            let count = 0;
+
+            emitter.on('test', () => { count += 1; });
+            expect(emitter.listenerCount('test')).toBe(1);
+
+            emitter.emit('test');
+
+            expect(count).toBe(1);
+        });
+
+        test('should notify on \'newListener\'', () => {
+            const emitter = new EventEmitter();
+            let reached = false;
+            const listener = () => {};
+
+            emitter.on('newListener', (eventName, ...listeners) => {
+                expect(eventName).toBe('test');
+                expect(listeners[0]).toBe(listener);
+                reached = true;
+            });
+
+            emitter.on('test', listener);
+
+            expect(reached).toBeTruthy();
+        });
+
+        test('should crash the process (no listeners on \'error\' event)', () => {
+            const emitter = new EventEmitter();
+
+            expect(() => {
+                emitter.emit('error');
+            }).toThrowError(new Error('no listeners on \'error\' event'));
+        });
+    });
+
+    /**
+     * emitter.setMaxListeners() and emitter.getMaxListeners()
+     */
+    describe('testing emitter.setMaxListener() & emitter.getMaxListener() methods', () => {
+        test('should initially be equal to the default amount', () => {
+            const emitter = new EventEmitter();
+
+            const res = emitter.getMaxListeners();
+            expect(res).toBe(EventEmitter.defaultMaxListeners);
+        });
+
+        test('should change the amount of max listener only for one instance', () => {
+            const emitter1 = new EventEmitter();
+            const emitter2 = new EventEmitter();
+            const newMaxListeners = 5;
+
+            emitter1.setMaxListeners(newMaxListeners);
+            expect(emitter1.getMaxListeners()).toBe(newMaxListeners);
+
+            expect(emitter2.getMaxListeners()).toBe(EventEmitter.defaultMaxListeners);
+        });
+
+        test('should throw TypeError when passing non number types', () => {
+            const emitter = new EventEmitter();
+
+            expect(() => {
+                emitter.setMaxListeners('asd');
+            }).toThrowError(new TypeError('the argument must be of type <number>'));
+        });
+
+        test('should truncate to nearest, non-negative, integer', () => {
+            const emitter = new EventEmitter();
+
+            emitter.setMaxListeners(11.5);
+            expect(emitter.getMaxListeners()).toBe(11);
+
+            emitter.setMaxListeners(-1);
+            expect(emitter.getMaxListeners()).toBe(Infinity);
+        });
+    });
+
+    /**
+     * emitter.listeners()
+     */
+    describe('testing emitter.listeners() method', () => {
+        test('should return an empty array on empty event', () => {
+            const emitter = new EventEmitter();
+
+            const listeners = emitter.listeners('error');
+
+            expect(Array.isArray(listeners)).toBeTruthy();
+            expect(listeners.length).toBe(0);
+        });
+
+        test('should return an empty array on non-existing event', () => {
+            const emitter = new EventEmitter();
+
+            const listeners = emitter.listeners('test');
+
+            expect(Array.isArray(listeners)).toBeTruthy();
+            expect(listeners.length).toBe(0);
+        });
+
+        test('should return a copy and not the original array', () => {
+            const emitter = new EventEmitter();
+
+            emitter.on('test', () => {});
+            emitter.on('test', () => {});
+            emitter.once('test', () => {});
+
+            const listeners = emitter.listeners('test');
+
+            expect(listeners.length).toBe(3);
+            expect(emitter.listenerCount('test')).toBe(3);
+
+            listeners.push(() => {});
+            expect(emitter.listenerCount('test')).toBe(3);
         });
     });
 });
